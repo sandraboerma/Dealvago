97a7c3e48fc6ece7b93396c58565909942b912aa
sandraszu <sandra.szu@iths.se>
2025-03-17 02:59:23 +0100

    Initial commit. Linking local project to GitHub

---
+++ src/main/java/com/boerma/dealvago/DealvagoApplication.java
@@ -0,0 +1,13 @@
+package com.boerma.dealvago;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DealvagoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DealvagoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/boerma/dealvago/DealvagoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boerma.dealvago;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DealvagoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
