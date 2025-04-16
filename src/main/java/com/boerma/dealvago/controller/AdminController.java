f26632b6c7745a043766de7d1cd95f18773ce65f
sandraszu <sandra.szu@iths.se>
2025-04-16 12:48:10 +0200

    Add adminpage and AdminController with basic shell structure

--- src/main/java/com/boerma/dealvago/controller/AdminController.java
+++ src/main/java/com/boerma/dealvago/controller/AdminController.java
@@ -1,4 +1,23 @@
 package com.boerma.dealvago.controller;
 
+import com.boerma.dealvago.domain.dto.ProductDto;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AdminController {
+
+    @GetMapping("/admin")
+    public String adminManagement(String view, Model model) {
+        model.addAttribute("view", view);
+        return "adminpage";
+    }
+
+    @PostMapping("/admin/products/add")
+    public String addProduct(String name, int unitPrice, int stock) {
+        //placeholder for adding product
+        return "adminpage";
+    }
 }
